77afaf3c4c3ee240077af626f7f67d70eb839254
yun <zhuyun@mvad.com>
2018-01-17 10:07:49 +0800

    service parameters update: 1. run in background, 2. use port 18080

--- scripts/run-service.sh
+++ scripts/run-service.sh
@@ -12,10 +12,10 @@ curr_dir=$(dirname $0)
 
 CMD="/bin/bash ${DOCKER_HOME}/bin/start-service.sh"
 ${DOCKER} ps -a | grep ${PROJECT_NAME} && ${DOCKER} stop ${PROJECT_NAME} && ${DOCKER} rm -v ${PROJECT_NAME}
-${DOCKER_ENGINE} run -it \
+${DOCKER_ENGINE} run -d \
   --net=bridge \
   --name ${PROJECT_NAME} \
-  -p 8080:8080 \
+  -p 18080:8080 \
   -v ${PROJECT_HOME}/log:${DOCKER_HOME}/log \
   ${DOCKER_TAG} ${CMD} \
   || { echo "fail to run ${DOCKER_TAG}" && exit 64; }
