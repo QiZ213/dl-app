ec6a793d540540fec54e0363e006ecb579b05661
zhuyun <zhuyun@ppdai.com>
2018-09-14 18:11:45 +0800

    add conflict file

---
+++ scripts/common_settings.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# project settings
+PROJECT_BIN=$(cd $(dirname ${BASH_SOURCE[0]}); pwd)
+PROJECT_HOME=$(cd $(dirname ${PROJECT_BIN}); pwd)
+source ${PROJECT_BIN}/common_utils.sh
+
+# system
+PYTHONPATH=${PROJECT_HOME}:${PYTHONPATH}
+GIT_BASE=git@git.ppdaicorp.com:$(whoami)
+
+# env settings
+PYTHON=python
+PYTHON_VERSION=2
+OS_VERSION=ubuntu14.04
+CUDA_VERSION=8.0
+CUDNN_VERSION=6
+DEEP_LEARNING_FRAMEWORK=tensorflow
+DEEP_LEARNING_VERSION=1.4.0
+
+# deployment settings
+# specify data dir to store raw data or persisted intermediate variables
+DATA_DIR=
+
+# specify log dir to store log
+LOG_DIR=
+
+# specify model dir to store models
+MODEL_DIR=
+
+# specify notebook dir to store .ipynb application,
+NOTEBOOK_DIR=
+
+# notebook settings
+# specify port for jupyter notebook service, by default, it's "18888"
+NOTEBOOK_PORT=18888
+
+# specify password for jupyter notebook service, by default, it's "123456"
+NOTEBOOK_PASSWORD=123456
+
+# application settings
+# define port for application service, by default, it's "18080"
+SERVING_PORT=18081
