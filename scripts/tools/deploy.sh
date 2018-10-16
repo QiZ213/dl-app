c8c9b984116fb87d5884ebfc01b2b0fcd690eae0
xieyuhan <xieyuhan@ppdai.com>
2018-10-16 15:51:33 +0800

    auto create TASK_HOME dir. fix build notebook error.

--- scripts/tools/deploy.sh
+++ scripts/tools/deploy.sh
@@ -46,13 +46,14 @@ link_dir() {
   fi
 }
 
+# TODO if TASK_TYPE == notebook or develop, this symlink is not useful.
 default_base_dir=/opt
 [[ -w ${default_base_dir} ]] || default_base_dir=~
 
-: ${DATA_DIR:=${default_base_dir}/data/${TASK_NAME}}
-: ${LOG_DIR:=${default_base_dir}/log/${TASK_NAME}}
-: ${MODEL_DIR:=${default_base_dir}/models/${TASK_NAME}}
-: ${NOTEBOOK_DIR:=${default_base_dir}/notebooks/${TASK_NAME}}
+: ${DATA_DIR:=${default_base_dir}/dl-data/${TASK_NAME}}
+: ${LOG_DIR:=${default_base_dir}/dl-log/${TASK_NAME}}
+: ${MODEL_DIR:=${default_base_dir}/dl-models/${TASK_NAME}}
+: ${NOTEBOOK_DIR:=${default_base_dir}/dl-notebooks/${TASK_NAME}}
 
 # link external dir
 link_dir ${DATA_DIR} ${USER_PROJECT_HOME}/data ${DRY_RUN}
