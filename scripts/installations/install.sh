3b94af78d5042f9cc24de3e96b733790473e78cb
xieyuhan <xieyuhan@ppdai.com>
2018-11-28 10:10:18 +0800

    fix code style

--- scripts/installations/install.sh
+++ scripts/installations/install.sh
@@ -11,13 +11,13 @@ source_existed() {
 install_base_required() {
   apt-get install -yq --no-install-recommends \
     libglib2.0-0 \
-  && pip install --no-cache-dir opencv-python==3.3.0.9
+    && pip install --no-cache-dir opencv-python==3.3.0.9
 }
 
 
 # install os requirements
 apt-get update \
-&& install_base_required \
-&& source_existed ${curr_dir}/install_custom.sh \
-&& apt-get clean \
-&& rm -rf /var/lib/apt/lists/*
+  && install_base_required \
+  && source_existed ${curr_dir}/install_custom.sh \
+  && apt-get clean \
+  && rm -rf /var/lib/apt/lists/*
